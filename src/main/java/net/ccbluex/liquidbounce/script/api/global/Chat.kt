50c9584dbf4960ea1ed31d2bc2dbeaed67a02a0a
NoneMinecraft <2771776476@qq.com>
2024-08-28 15:51:26 +0800

    Add files via upload

---
+++ src/main/java/net/ccbluex/liquidbounce/script/api/global/Chat.kt
@@ -0,0 +1,26 @@
+
+package net.ccbluex.liquidbounce.script.api.global
+
+import net.ccbluex.liquidbounce.utils.ClientUtils
+
+/**
+ * Object used by the script API to provide an easier way of calling chat-related methods.
+ */
+object Chat {
+
+    /**
+     * Prints a message to the chat (client-side)
+     * @param message Message to be printed
+     */
+    @Suppress("unused")
+    @JvmStatic
+    fun print(message: String) {
+        ClientUtils.displayChatMessage(message)
+    }
+
+    @Suppress("unused")
+    @JvmStatic
+    fun alert(message: String) {
+        ClientUtils.displayAlert(message)
+    }
+}
