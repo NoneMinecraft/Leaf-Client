ba139d7ca115bc6e88ffb5e544e14f503289b615
NoneMinecraft <2771776476@qq.com>
2025-02-18 22:40:08 +0800

    Add files via upload

--- src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/mainmenu/ModernGuiMainMenu.java
+++ src/main/java/net/ccbluex/liquidbounce/launch/data/modernui/mainmenu/ModernGuiMainMenu.java
@@ -1,82 +1,91 @@
-/*
-by None
-仿制非打滑
- */
 package net.ccbluex.liquidbounce.launch.data.modernui.mainmenu;
 
-import net.ccbluex.liquidbounce.utils.*;
 import net.ccbluex.liquidbounce.ui.client.altmanager.*;
+import net.ccbluex.liquidbounce.utils.MainMenuButton;
 import net.minecraft.client.gui.*;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.util.*;
 import net.ccbluex.liquidbounce.utils.render.*;
 import java.awt.*;
-import net.ccbluex.liquidbounce.font.*;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class ModernGuiMainMenu extends GuiScreen {
-    public ArrayList<MainMenuButton> butt;
+    private final ArrayList<MainMenuButton> buttons = new ArrayList<>();
+    private MainMenuButton toggleUIButton; // UI切换按钮
     private ScaledResolution res;
-    private float currentX;
-    private float currentY;
+    private float sidebarX = -120.0f;
+    private boolean hoveringSidebar = false;
+    private final float sidebarWidth = 120.0f;
+    private final float sidebarHeight = 250.0f;
+    private final float animationSpeed = 5.0f;
+    private boolean isAlternativeLayout = false;
+    private boolean isLoading = false;
+    private long loadingStartTime = 0;
+    private ResourceLocation backgroundImage = new ResourceLocation("leaf/background.png");
+    private ResourceLocation image = new ResourceLocation("leaf/image.png");
+    public ModernGuiMainMenu() {}
 
-    public ModernGuiMainMenu() {
-        this.butt = new ArrayList<MainMenuButton>();
-    }
-
+    @Override
     public void initGui() {
-        this.butt.clear();
-        final float buttonWidth = 50.0f;
-        final float buttonHeight = 22.0f;
-        final float startX = this.width / 2.0f - buttonWidth / 2.0f;
-        final float startY = this.height / 2.0f - 110.0f;
-        this.butt.add(new MainMenuButton(this, 0, "G", "Single", () -> this.mc.displayGuiScreen(new GuiSelectWorld(this)), buttonWidth, buttonHeight, startX - 120.0f, startY + 104.0f));
-        this.butt.add(new MainMenuButton(this, 1, "H", "Multi", () -> this.mc.displayGuiScreen(new GuiMultiplayer(this)), buttonWidth, buttonHeight, startX - 60.0f, startY + 104.0f));
-        this.butt.add(new MainMenuButton(this, 2, "I", "Alt", () -> this.mc.displayGuiScreen(new GuiAltManager(this)), buttonWidth, buttonHeight, startX, startY + 104.0f));
-        this.butt.add(new MainMenuButton(this, 4, "K", "Option", () -> this.mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings)), buttonWidth, buttonHeight, startX + 60.0f, startY + 104.0f));
-        this.butt.add(new MainMenuButton(this, 5, "L", "Language", () -> this.mc.displayGuiScreen(new GuiLanguage(this, this.mc.gameSettings, this.mc.getLanguageManager())), buttonWidth, buttonHeight, startX + 120.0f, startY + 104.0f));
-
-        this.res = new ScaledResolution(this.mc);
+        this.buttons.clear();
+        res = new ScaledResolution(this.mc);
+        initDefaultLayout();
         super.initGui();
     }
 
+    private void initDefaultLayout() {
+        final float buttonWidth = 100.0f;
+        final float buttonHeight = 15.0f;
+        final float startY = (this.height - sidebarHeight) / 2.0f + 20.0f;
+
+        this.buttons.add(new MainMenuButton(this, 0, "G", "Single", () -> this.mc.displayGuiScreen(new GuiSelectWorld(this)), buttonWidth, buttonHeight, 10, startY));
+        this.buttons.add(new MainMenuButton(this, 1, "H", "Multi", () -> this.mc.displayGuiScreen(new GuiMultiplayer(this)), buttonWidth, buttonHeight, 10, startY + 30.0f));
+        this.buttons.add(new MainMenuButton(this, 2, "I", "Alt", () -> this.mc.displayGuiScreen(new GuiAltManager(this)), buttonWidth, buttonHeight, 10, startY + 60.0f));
+        this.buttons.add(new MainMenuButton(this, 3, "K", "Option", () -> this.mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings)), buttonWidth, buttonHeight, 10, startY + 90.0f));
+        this.buttons.add(new MainMenuButton(this, 4, "L", "Language", () -> this.mc.displayGuiScreen(new GuiLanguage(this, this.mc.gameSettings, this.mc.getLanguageManager())), buttonWidth, buttonHeight, 10, startY + 120.0f));
+    }
+
+    @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        try {
-            this.drawGradientRect(0, 0, this.width, this.height, 16777215, 16777215);
-            int h = this.height;
-            int w = this.width;
-            float xDiff = ((float) (mouseX - h / 2) - this.currentX) / (float) this.res.getScaleFactor();
-            float yDiff = ((float) (mouseY - w / 2) - this.currentY) / (float) this.res.getScaleFactor();
-            this.currentX += xDiff * 0.3F;
-            this.currentY += yDiff * 0.3F;
-            GlStateManager.translate(this.currentX / 30.0F, this.currentY / 15.0F, 0.0F);
-
-            RenderUtils.drawImage(new ResourceLocation("leaf/background.png"), 0, 0, this.res.getScaledWidth(), this.res.getScaledHeight());
-
-            GlStateManager.translate(-this.currentX / 30.0F, -this.currentY / 15.0F, 0.0F);
-
-            RenderUtils.drawRoundedCornerRect(this.width / 2.0F - 150.0F, this.height / 2.0F - 10.0F, this.width / 2.0F + 150.0F, this.height / 2.0F + 20.0F, 5.0f, new Color(0, 0, 0, 80).getRGB());
-            FontLoaders.F20.drawCenteredString("Leaf Client", this.width / 2.0F, this.height / 2.0F - 25.0F, new Color(255, 255, 255).getRGB());
-
-            for (final MainMenuButton button : this.butt) {
-                button.draw(mouseX, mouseY);
+        if (isLoading) {
+            long elapsedTime = System.currentTimeMillis() - loadingStartTime;
+            if (elapsedTime >= 1000) {
+                isLoading = false;
+                isAlternativeLayout = !isAlternativeLayout;
+                backgroundImage = new ResourceLocation("leaf/background.png");
+                initGui();
+            } else {
+                int alpha = (int) ((elapsedTime / 1000.0f) * 255);
+                this.drawGradientRect(0, 0, this.width, this.height, new Color(0, 0, 0, alpha).getRGB(), new Color(0, 0, 0, alpha).getRGB());
+                return;
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
+
+        this.drawGradientRect(0, 0, this.width, this.height, 0xFF202020, 0xFF202020);
+        RenderUtils.drawImage(backgroundImage, 0, 0, this.res.getScaledWidth(), this.res.getScaledHeight());
+        if (!isAlternativeLayout) {
+            hoveringSidebar = mouseX < sidebarWidth && mouseY > (this.height - sidebarHeight) / 2 && mouseY < (this.height + sidebarHeight) / 2;
+            sidebarX += (hoveringSidebar ? animationSpeed : -animationSpeed);
+            sidebarX = Math.max(-sidebarWidth, Math.min(0, sidebarX));
+            GlStateManager.pushMatrix();
+            GlStateManager.translate(sidebarX, 0, 0);
+        }
+
+        for (MainMenuButton button : buttons) {
+            button.draw(mouseX, mouseY);
+        }
+
+        if (!isAlternativeLayout) {
+            GlStateManager.popMatrix();
+        }
+
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
-
+    @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        for (final MainMenuButton button : this.butt) {
+        for (MainMenuButton button : buttons) {
             button.mouseClick(mouseX, mouseY, mouseButton);
         }
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
-    public void updateScreen() {
-        this.res = new ScaledResolution(this.mc);
-        super.updateScreen();
     }
 }
