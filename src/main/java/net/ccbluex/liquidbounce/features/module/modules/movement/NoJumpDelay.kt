0a2aa7c6e101dece29d145a3438be84cc1d9f811
NoneMinecraft <2771776476@qq.com>
2025-02-18 22:36:08 +0800

    Add files via upload

---
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/NoJumpDelay.kt
@@ -0,0 +1,12 @@
+/*
+ * Leaf Hacked Client
+ * Code by NoneMinecraft
+ */
+package net.ccbluex.liquidbounce.features.module.modules.movement
+
+import net.ccbluex.liquidbounce.features.module.Module
+import net.ccbluex.liquidbounce.features.module.ModuleCategory
+import net.ccbluex.liquidbounce.features.module.ModuleInfo
+
+@ModuleInfo(name = "NoJumpDelay", category = ModuleCategory.MOVEMENT)
+class NoJumpDelay : Module()
