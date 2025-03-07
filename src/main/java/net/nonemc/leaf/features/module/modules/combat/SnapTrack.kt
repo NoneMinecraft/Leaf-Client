7950a934cd9c91d8b637e342e382f81feeb4a41f
NoneMinecraft <2771776476@qq.com>
2025-03-07 22:30:36 +0800

    SnapTrack

--- src/main/java/net/nonemc/leaf/features/module/modules/combat/SnapTrack.kt
+++ src/main/java/net/nonemc/leaf/features/module/modules/combat/SnapTrack.kt
@@ -1,97 +1,101 @@
 ﻿package net.nonemc.leaf.features.module.modules.combat
 
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.network.play.client.C02PacketUseEntity
+import net.minecraft.network.play.client.C03PacketPlayer
+import net.minecraft.network.play.server.S14PacketEntity
+import net.minecraft.network.play.server.S18PacketEntityTeleport
 import net.nonemc.leaf.event.EventTarget
-import net.nonemc.leaf.event.Render3DEvent
+import net.nonemc.leaf.event.PacketEvent
 import net.nonemc.leaf.event.UpdateEvent
 import net.nonemc.leaf.features.module.Module
 import net.nonemc.leaf.features.module.ModuleCategory
 import net.nonemc.leaf.features.module.ModuleInfo
 import net.nonemc.leaf.utils.extensions.getDistanceToEntityBox
+import net.nonemc.leaf.utils.timer.MSTimer
 import net.nonemc.leaf.value.BoolValue
 import net.nonemc.leaf.value.FloatValue
-import net.nonemc.leaf.value.ListValue
+import net.nonemc.leaf.value.IntegerValue
 
 
 @ModuleInfo(name = "SnapTrack", category = ModuleCategory.COMBAT)
 class SnapTrack : Module() {
+    private val delay = IntegerValue("Delay",200,1,2000)
+    private val speed = FloatValue("Speed",0.6f,0.01f,1f)
     private val maxRange = FloatValue("MaxRange", 5f, 0f, 10f)
+    private val minRange = FloatValue("MinRange", 2.8f, 0f, 10f)
     private val attackRange = FloatValue("AttackRange", 3f, 0f, 10f)
     private val predictSize = FloatValue("PredictSize", 1f, 1f, 5f)
-    private val posXZOffset = FloatValue("PosXZOffset", 0.1f, -1f, 1f)
-    private val posYOffset = FloatValue("PosYOffset", 0.0f, -1f, 1f)
-    private val ignoreSmoothing = BoolValue("IgnoreSmoothing", false)
     private val onlySwing = BoolValue("OnlySwing", false)
-    private val positionProcessingMode = ListValue(
-        "PositionProcessingMode",
-        arrayOf("Pos", "ServerPos", "MiddlePos", "MixPos", "CustomPosOffset"),
-        "MiddlePos"
-    )
-
-    @EventTarget
-    fun onRender3D(event: Render3DEvent) {
-        if (ignoreSmoothing.get()) runSetTargetSPos()
+    private val s14 = BoolValue("S14", true)
+    private val s18 = BoolValue("S18", true)
+    private val cancelC03 = BoolValue("CancelC03", false)
+    private val cancelC02 = BoolValue("CancelC02", true)
+    private var canTP = false
+    private var canCancel = false
+    private val tpTimer = MSTimer()
+    private var targetPlayer:EntityPlayer? = null
+    var targetVecX = 0.0
+    var targetVecY = 0.0
+    var targetVecZ = 0.0
+    override fun onDisable() {
+        canTP = false
+        canCancel = false
+        tpTimer.reset()
     }
-
     @EventTarget
     fun onUpdate(event: UpdateEvent) {
-        runSetTargetSPos()
+        if (targetPlayer != null && canTP){
+            if (tpTimer.hasTimePassed(delay.get().toLong())){
+                canTP = false
+                canCancel = false
+                tpTimer.reset()
+            } else {
+                canCancel = true
+                targetPlayer!!.setPosition(
+                    targetPlayer!!.posX + (targetVecX - targetPlayer!!.posX) * speed.get(),
+                    targetPlayer!!.posY + (targetVecY - targetPlayer!!.posY) * speed.get(),
+                    targetPlayer!!.posZ + (targetVecZ - targetPlayer!!.posZ) * speed.get()
+                )
+            }
+        }
     }
 
-    private fun runSetTargetSPos() {
+    @EventTarget
+    fun onPacket(event: PacketEvent) {
         val player = mc.thePlayer ?: return
         val world = mc.theWorld ?: return
+        val packet = event.packet
+        if (packet is S14PacketEntity){
+            if (canCancel && packet.getEntity(world) == targetPlayer && s14.get()) event.cancelEvent()
+        }
+        if (packet is S18PacketEntityTeleport){
+            if (packet.entityId == targetPlayer!!.entityId && canCancel && s18.get()) event.cancelEvent()
+        }
         for (target in world.playerEntities) {
-            if (target !== mc.thePlayer) {
+            if (target !== player) {
                 val distance = player.getDistanceToEntityBox(target)
-                if (distance <= maxRange.get()) {
-                    val targetVecX = target.posX + (target.posX - target.prevPosX) * predictSize.get()
-                    val targetVecY = target.posY + (target.posY - target.prevPosY) * predictSize.get()
-                    val targetVecZ = target.posZ + (target.posZ - target.prevPosZ) * predictSize.get()
+                if (distance in minRange.get()..maxRange.get()) {
+                    targetVecX = target.posX + (target.posX - target.prevPosX) * predictSize.get()
+                    targetVecY = target.posY + (target.posY - target.prevPosY) * predictSize.get()
+                    targetVecZ = target.posZ + (target.posZ - target.prevPosZ) * predictSize.get()
                     if (player.getDistance(targetVecX, targetVecY, targetVecZ) <= attackRange.get()) {
-                        if (!onlySwing.get() || mc.thePlayer.isSwingInProgress) target.setPositionAndUpdate(
-                            targetVecX,
-                            targetVecY,
-                            targetVecZ
-                        )
-                    } else {
-                        val posX = target.posX
-                        val posY = target.posY
-                        val posZ = target.posZ
-                        val sPosX = target.serverPosX.toDouble() / 32
-                        val sPosY = target.serverPosY.toDouble() / 32
-                        val sPosZ = target.serverPosZ.toDouble() / 32
-                        when (positionProcessingMode.get()) {
-                            "ServerPos" -> target.setPositionAndUpdate(sPosX, sPosY, sPosZ)
-                            "MiddlePos" -> {
-                                val middlePosX = (posX + sPosX) / 2
-                                val middlePosY = (posY + sPosY) / 2
-                                val middlePosZ = (posZ + sPosZ) / 2
-                                target.setPositionAndUpdate(middlePosX, middlePosY, middlePosZ)
+                        if (!onlySwing.get() || player.isSwingInProgress) {
+                            if (packet is C03PacketPlayer && cancelC03.get()){
+                                event.cancelEvent()
                             }
+                            if (packet is C02PacketUseEntity && cancelC02.get()){
+                                event.cancelEvent()
+                            }
+                            tpTimer.reset()
+                            targetPlayer = target
 
-                            "MixPos" -> {
-                                val mixPosX = posX + (sPosX - posX)
-                                val mixPosY = posY + (sPosY - posY)
-                                val mixPosZ = posZ + (sPosZ - posZ)
-                                target.setPositionAndUpdate(mixPosX, mixPosY, mixPosZ)
-                            }
-
-                            "CustomPosOffset" -> {
-                                val middlePosX = (posX + sPosX) / 2
-                                val middlePosY = (posY + sPosY) / 2
-                                val middlePosZ = (posZ + sPosZ) / 2
-                                target.setPositionAndUpdate(
-                                    middlePosX + posXZOffset.get(),
-                                    middlePosY + posYOffset.get(),
-                                    middlePosZ + posXZOffset.get()
-                                )
-                                //Pos直接跳过
-                            }
+                            canCancel = true
+                            canTP = true
                         }
                     }
                 }
             }
         }
     }
-
 }
