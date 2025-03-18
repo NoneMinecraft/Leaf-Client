ee3a7713045d06fb3b16a3995b6b914736e9bbe2
NoneMinecraft <2771776476@qq.com>
2025-03-18 21:14:17 +0800

    Add files via upload

---
+++ src/main/java/net/nonemc/leaf/injection/forge/mixins/block/MixinBlockSoulSand.java
@@ -0,0 +1,21 @@
+package net.nonemc.leaf.injection.forge.mixins.block;
+
+import net.nonemc.leaf.Leaf;
+import net.nonemc.leaf.features.module.modules.movement.NoSlow;
+import net.minecraft.block.BlockSoulSand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockSoulSand.class)
+public class MixinBlockSoulSand {
+
+    @Inject(method = "onEntityCollidedWithBlock", at = @At("HEAD"), cancellable = true)
+    private void onEntityCollidedWithBlock(CallbackInfo callbackInfo) {
+        final NoSlow noSlow = Leaf.moduleManager.getModule(NoSlow.class);
+
+        if(noSlow.getState() && noSlow.getSoulsandValue().get())
+            callbackInfo.cancel();
+    }
+}
---
+++ src/main/java/net/nonemc/leaf/injection/forge/mixins/render/MixinVisGraph.java
@@ -0,0 +1,19 @@
+package net.nonemc.leaf.injection.forge.mixins.render;
+
+import net.nonemc.leaf.Leaf;
+import net.nonemc.leaf.features.module.modules.render.XRay;
+import net.minecraft.client.renderer.chunk.VisGraph;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(VisGraph.class)
+public class MixinVisGraph {
+
+    @Inject(method = "func_178606_a", at = @At("HEAD"), cancellable = true)
+    private void func_178606_a(final CallbackInfo callbackInfo) {
+        if (Leaf.moduleManager.getModule(XRay.class).getState())
+            callbackInfo.cancel();
+    }
+}
