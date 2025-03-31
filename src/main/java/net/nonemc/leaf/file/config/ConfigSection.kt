8514268b30c7f5503cb0c8b7241540db333b92dd
NoneMinecraft <2771776476@qq.com>
2025-03-31 22:06:53 +0800

    Delete src/main/java/net/nonemc/leaf/file directory

--- src/main/java/net/nonemc/leaf/file/config/ConfigSection.kt
+++
@@ -1,9 +0,0 @@
-﻿package net.nonemc.leaf.file.config
-
-import com.google.gson.JsonObject
-
-abstract class ConfigSection(val sectionName: String) {
-    abstract fun load(json: JsonObject)
-
-    abstract fun save(): JsonObject
-}
