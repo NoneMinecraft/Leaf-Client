6551bc5890fde8a7c30856a9fab48a31a1efab59
NoneMinecraft <2771776476@qq.com>
2025-03-31 22:16:22 +0800

    Delete src/main/java/net/nonemc/leaf/injection directory

--- src/main/java/net/nonemc/leaf/injection/forge/mixins/performance/MixinEntityFX.java
+++
@@ -1,9 +0,0 @@
-package net.nonemc.leaf.injection.forge.mixins.performance;
-
-import net.minecraft.client.particle.EntityFX;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(value={EntityFX.class})
-public class MixinEntityFX {
-
-}
