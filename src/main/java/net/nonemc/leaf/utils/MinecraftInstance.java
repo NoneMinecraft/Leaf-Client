2764266a8b03935cbba709c26290e5e129834614
NoneMinecraft <2771776476@qq.com>
2025-03-31 22:01:00 +0800

    Delete src/main/java/net/nonemc/leaf/utils directory

--- src/main/java/net/nonemc/leaf/utils/MinecraftInstance.java
+++
@@ -1,7 +0,0 @@
-package net.nonemc.leaf.utils;
-
-import net.minecraft.client.Minecraft;
-
-public class MinecraftInstance {
-    protected static final Minecraft mc = Minecraft.getMinecraft();
-}
--- src/main/java/net/nonemc/leaf/utils/MinecraftInstance.kt
+++
@@ -1,5 +0,0 @@
-﻿package net.nonemc.leaf.utils
-
-import net.minecraft.client.Minecraft
-
-val mc = Minecraft.getMinecraft()
