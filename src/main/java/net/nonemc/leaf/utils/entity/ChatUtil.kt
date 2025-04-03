c5a3bb42a1eb6a4051ac5eac3aec6c248a0b4c79
NoneMinecraft <2771776476@qq.com>
2025-04-03 21:32:26 +0800

    Add files via upload

---
+++ src/main/java/net/nonemc/leaf/utils/entity/ChatUtil.kt
@@ -0,0 +1,20 @@
+﻿package net.nonemc.leaf.utils.entity
+
+import net.minecraft.network.play.client.C01PacketChatMessage
+import net.minecraft.util.ChatComponentText
+import net.nonemc.leaf.utils.mc
+
+object ChatUtil {
+    fun Chat(message: String) {
+        mc.thePlayer.sendQueue.addToSendQueue(C01PacketChatMessage(message))
+    }
+
+    fun tell(player: String, message: String) {
+        mc.thePlayer.sendQueue.addToSendQueue(C01PacketChatMessage("/tell $player $message"))
+    }
+
+    fun ChatPrint(message: String) {
+        mc.ingameGUI.chatGUI.printChatMessage(ChatComponentText(message))
+    }
+
+}
