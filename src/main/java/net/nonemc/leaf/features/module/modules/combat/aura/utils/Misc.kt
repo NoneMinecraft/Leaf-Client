66c4725b6f0d86f32886f31cb32d9e636f73b580
NoneMinecraft <2771776476@qq.com>
2025-04-26 00:19:31 +0800

    Delete src/main/java/net/nonemc/leaf/features/module/modules/combat directory

--- src/main/java/net/nonemc/leaf/features/module/modules/combat/aura/utils/Misc.kt
+++
@@ -1,5 +0,0 @@
-﻿package net.nonemc.leaf.features.module.modules.combat.aura.utils
-
-fun findClosestValue(values: List<Double>, target: Double): Double {
-    return values.minByOrNull { Math.abs(it - target) } ?: values[0]
-}
