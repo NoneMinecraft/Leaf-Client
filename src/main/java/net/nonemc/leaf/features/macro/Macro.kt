bbd4eff3578491808bda710b9d820337e030509c
NoneMinecraft <2771776476@qq.com>
2025-04-26 00:16:31 +0800

    Delete src/main/java/net/nonemc/leaf/features/macro directory

--- src/main/java/net/nonemc/leaf/features/macro/Macro.kt
+++
@@ -1,9 +0,0 @@
-﻿package net.nonemc.leaf.features.macro
-
-import net.nonemc.leaf.Leaf
-
-class Macro(val key: Int, val command: String) {
-    fun exec() {
-        Leaf.commandManager.executeCommands(command)
-    }
-}
