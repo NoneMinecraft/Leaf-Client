4a84a5585db12b4fc0e1df4a5b43472f8add3a94
NoneMinecraft <2771776476@qq.com>
2025-04-26 00:12:56 +0800

    Delete src/main/java/net/nonemc/leaf/launch directory

--- src/main/java/net/nonemc/leaf/launch/data/modernui/clickgui/fonts/api/FontFamily.java
+++
@@ -1,8 +0,0 @@
-package net.nonemc.leaf.launch.data.modernui.clickgui.fonts.api;
-
-public interface FontFamily {
-
-    FontRenderer ofSize(int size);
-
-    FontType font();
-}
--- src/main/java/net/nonemc/leaf/launch/data/modernui/clickgui/utils/normal/Utils.java
+++
@@ -1,9 +0,0 @@
-package net.nonemc.leaf.launch.data.modernui.clickgui.utils.normal;
-
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
-
-public interface Utils {
-    Minecraft mc = Minecraft.getMinecraft();
-    FontRenderer fr = mc.fontRendererObj;
-}
